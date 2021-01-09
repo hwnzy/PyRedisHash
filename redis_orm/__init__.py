b6a12aad92ec9f4f56b7fb814a6b241c12bc3827
ningzhiying <hwnzy@foxmail.com>
2021-01-09 23:01:07 +0800

    Finish Model.

--- redis_orm/__init__.py
+++ redis_orm/__init__.py
@@ -1,22 +1,63 @@
 # -*- coding: utf-8 -*-
+import re
+
+from redis_orm.field import _Field
+
+
+def camel_case_to_underscore_case(strs):
+    pattern = re.compile(r'[A-Z][0-9a-z_]*')
+    r = pattern.findall(strs)
+    return '_'.join(map(lambda x: x.lowers(), r))
+
 
 class ModelMetaclass(type):
-    def __new__(cls, *args, **kwargs):
-        pass
+    def __new__(mcs, name, bases, attrs):
+        if name in ('Model', 'RedisHash'):
+            return type.__new__(mcs, name, bases, attrs)
+        db_name = attrs.get('__db_name__', camel_case_to_underscore_case(name))
+        mappings = {}
+        fields = []
+        
+        for k, v in attrs.items():
+            if isinstance(k, _Field):
+                mappings[k] = v
+                fields.append(k)
+            
+        for k in fields:
+            attrs.pop(k)
+        
+        for base in bases:
+            if issubclass(base, RedisHash) and base != RedisHash and hasattr(base, "__mappings__"):
+                for k, v in base.__mappings__.items():
+                    if k not in mappings:
+                        mappings[k] = v
+
+        attrs["__db_name__"] = db_name
+        attrs["__mappings__"] = mappings
+
+        return type.__new__(mcs, name, bases, attrs)
 
 
 class Model(dict, metaclass=ModelMetaclass):
-    def __init__(self):
-        pass
+
+    def __init__(self, **data):
+        super(Model, self).__init__(data)
 
     def __getattr__(self, item):
-        pass
+        try:
+            return self[item]
+        except KeyError:
+            pass
 
     def __setattr__(self, key, value):
-        pass
+        if key in self.__mappings__:
+            self[key] = value
+        else:
+            self.__dict__[key] = value
 
     def save(self):
         pass
+
 
 class RedisHash(Model):
     def __init__(self):
